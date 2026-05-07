aa939e3890ec2f7b086e57d64cffe1aada854fba
michspuch <121891333+michspuch@users.noreply.github.com>
2026-05-07 00:07:22 -0400

    Update 03.Fst.R

--- 03.Fst.R
+++ 03.Fst.R
@@ -9,6 +9,12 @@ fst_summary <- tribble(
   ~pop1, ~pop2, ~mean_fst,
   "historical", "contemporary", 0.024797)
 
+#Bind the fst summary data
+fst_plot <- bind_rows(
+  fst_summary,
+  fst_summary %>% rename(pop1 = pop2, pop2 = pop1)
+)
+
 #Set up plot
 ggplot(fst_plot, aes(x = pop1, y = pop2, fill = mean_fst)) +
   geom_tile() +
@@ -21,5 +27,28 @@ ggplot(fst_plot, aes(x = pop1, y = pop2, fill = mean_fst)) +
     fill = "Mean FST"
   ) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#Assign table for min, max, mean, and mode FST
+pairwise_fst <- read_table("/Users/classes/bio355b/CURE_projects/temporal/results/Jensen_pairwise_fst.weir.fst")
+
+#Check to make sure it works
+head(pairwise_fst)
+
+#Calculate min, max, mean FST
+min(pairwise_fst$WEIR_AND_COCKERHAM_FST)
+max(pairwise_fst$WEIR_AND_COCKERHAM_FST)
+mean(pairwise_fst$WEIR_AND_COCKERHAM_FST)
+
+#Mode FST
+# Define the function
+get_mode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+# Apply to a specific column (e.g., 'my_column' in data frame 'df')
+result <- get_mode(pairwise_fst$WEIR_AND_COCKERHAM_FST)
+print(result)
+
 
   
